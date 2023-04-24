8066ad2a8d6bef07dd6d81d164b92de173ad51fd
Michael Matasyants <michael.matasyanc@gmail.com>
2023-04-24 17:30:20 +0300

    Add create_box()

--- bot_dev/db.py
+++ bot_dev/db.py
@@ -1,6 +1,4 @@
-import os
 import mysql.connector
-from dotenv import load_dotenv
 
 
 def create_connection(host_name, database, user_name, user_password):
@@ -14,17 +12,35 @@ def create_connection(host_name, database, user_name, user_password):
         return print(f"The error '{err}' occurred")
     return connection
 
+# data_user = [tg_username, nickname, phone, adress]
+def create_or_get_user(connection: mysql.connector,
+                       data_user: list) -> int | None:
+    tg_username = data_user[1]
+    cursor = connection.cursor()
+    try:
+        get_user_id = ("SELECT user_id FROM user "
+                       "WHERE tg_username = %s")
+        cursor.execute(get_user_id, (tg_username, ))
+        user_id = cursor.fetchall()[0][0]
+        return user_id
+    except mysql.connector.Error:
+        add_user = ("INSERT INTO user "
+                    "(tg_username, nickname, phone, adress) "
+                    "VALUES (%s, %s, %s, %s)")
+        cursor.execute(add_user, data_user)
+        connection.commit()
 
-def create_user(cnx: mysql.connector, data_user) -> None:
-    add_user = ("INSERT INTO user "
-                "(tg_user_name, nickname, phone, adress) "
-                "VALUES (%s, %s, %s, %s)")
-    cursor = cnx.cursor()
-    cursor.execute(add_user, data_user)
-    cnx.commit()
 
-def create_box()  -> None:
-    pass
+# data_box = [box_name, created_at, finished_at, items_size, items_weight, salt, encrypted_key]
+def create_box(connection: mysql.connector, data_user: list, data_box: list):
+    user_id = create_or_get_user(connection, data_user)
+    data_box.insert(1, user_id)
+    add_box_for_user = ("INSERT INTO box "
+                        "(box_name, user_id, created_at, finished_at, items_size, items_weight, salt, encrypted_key) "
+                        "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
+    cursor = connection.cursor()
+    cursor.execute(add_box_for_user, data_box)
+    connection.commit()
 
 
 def get_user_boxes(user_id: int) -> list[tuple]:
@@ -44,8 +60,9 @@ def get_overdue_boxes() -> list[tuple]:
 
 
 def get_overdue_box(box_id: int):
-    return user.phone, box.finished_at, 'etc..'
+    return # user.phone, box.finished_at, 'etc..'
 
 
 def get_all_boxes() -> list[tuple]:
     pass
+
